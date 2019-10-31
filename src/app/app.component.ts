17ffb958813a1de9e4b821fb5aedfdf45a04aac7
RAFIKI Houssam <houssam-eddine.rafiki@findl.lu>
2019-10-31 17:56:08 +0100

    first version work with one enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as moment from 'moment';
+import 'moment/locale/fr';
+
+moment.locale('fr');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ltco-time';
+export class AppComponent implements OnInit {
+  today: moment.Moment;
+  defaultHours = '00';
+  defaultMinutes = '00';
+  exit: moment.Moment;
+
+  ngOnInit(): void {
+    // set today date
+    this.today = moment();
+    // set default work hours Lundi et Vendredi 8h30, Mardi au Jeudi 9h, week-and 0h
+    if (this.today.day() === 1 || this.today.day() === 5) {
+      this.defaultHours = '09';
+    } else if (this.today.day() > 1 && this.today.day() < 5) {
+      this.defaultHours = '09';
+      this.defaultMinutes = '30';
+    }
+  }
+
+  onSelectHour(hour: string) {
+    const enter = moment(hour, 'HH:mm');
+    this.exit = enter.add(+this.defaultHours, 'h').add(+this.defaultMinutes, 'm');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import {MDBBootstrapModule} from 'angular-bootstrap-md';
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    NgxMaterialTimepickerModule.setLocale('fr-FR')
   ],
   providers: [],
   bootstrap: [AppComponent]
